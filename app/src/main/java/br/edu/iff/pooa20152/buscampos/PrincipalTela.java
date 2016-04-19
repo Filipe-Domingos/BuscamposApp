80b658bde73200fcb4025767d5f75699b082ab16
Filipe Roberto <filipe-domingos@hotmail.com>
2016-04-19 18:15:13 -0300

    Correção de importação

--- app/src/main/java/br/edu/iff/pooa20152/buscampos/PrincipalTela.java
+++ app/src/main/java/br/edu/iff/pooa20152/buscampos/PrincipalTela.java
@@ -1,7 +1,9 @@
 package br.edu.iff.pooa20152.buscampos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PrincipalTela extends AppCompatActivity {
 
