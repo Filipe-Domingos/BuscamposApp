f4add58a92119378b74413f34c4a7557f01a2c87
Filipe Roberto <filipe-domingos@hotmail.com>
2016-04-19 17:29:34 -0300

    Correção da Classe CadastroOnibusTela.java

--- app/src/main/java/br/edu/iff/pooa20152/buscampos/CadastroOnibusTela.java
+++ app/src/main/java/br/edu/iff/pooa20152/buscampos/CadastroOnibusTela.java
@@ -2,6 +2,8 @@ package br.edu.iff.pooa20152.buscampos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CadastroOnibusTela extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class CadastroOnibusTela extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_onibus_tela);
+
+        //Seleção Avaliação
+        Spinner spinner = (Spinner) findViewById(R.id.avaliacao_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.avaliacao_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        //Seleção Assento Especial
+        Spinner spinner1 = (Spinner) findViewById(R.id.assento_spinner);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.assento_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
     }
 }
