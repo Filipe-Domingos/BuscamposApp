0dd9d8e5c9fa1febbd9bea64d19d96bdf5404509
Filipe Roberto <filipe-domingos@hotmail.com>
2016-04-19 17:27:08 -0300

    Correção da Classe CadastroUsuarioTela.java

--- app/src/main/java/br/edu/iff/pooa20152/buscampos/CadastroUsuarioTela.java
+++ app/src/main/java/br/edu/iff/pooa20152/buscampos/CadastroUsuarioTela.java
@@ -3,6 +3,8 @@ package br.edu.iff.pooa20152.buscampos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CadastroUsuarioTela extends AppCompatActivity {
 
@@ -13,5 +15,15 @@ public class CadastroUsuarioTela extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cadastro_usuario_tela);
         Log.i(CICLO, "Testando Log");
+
+        //Seleção Sexo
+        Spinner spinner = (Spinner) findViewById(R.id.sexo_spinner);
+        // Criar um ArrayAdapter usando a matriz de cadeia e um layout padrão girador
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.sexo_array, android.R.layout.simple_spinner_item);
+        // Especifique o layout a ser usado quando a lista de opções aparece
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Aplique o adaptador para o spinner
+        spinner.setAdapter(adapter);
     }
 }
