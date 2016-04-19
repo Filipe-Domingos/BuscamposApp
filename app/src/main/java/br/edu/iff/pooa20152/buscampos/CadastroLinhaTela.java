c25b6a76b850c40cf28a0ab4e9f024bead7fd993
Filipe Roberto <filipe-domingos@hotmail.com>
2016-04-19 17:37:09 -0300

    Criando classe CadastroLinhaTela.java e Activity cadastro_linha_tela.xml

---
+++ app/src/main/java/br/edu/iff/pooa20152/buscampos/CadastroLinhaTela.java
@@ -0,0 +1,13 @@
+package br.edu.iff.pooa20152.buscampos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroLinhaTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastro_linha_tela);
+    }
+}
